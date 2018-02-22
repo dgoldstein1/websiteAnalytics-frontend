fcfc8d49b8e9c33b6fdbda32b2d1d5b9d1a4b9f3
David Goldstein <dgoldstein01@gmail.com>
2018-02-22 18:45:13 -0500

    - map is showing data!

--- src/components/map.js
+++ src/components/map.js
@@ -4,58 +4,66 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // leaflet
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup, CircleMarker } from 'react-leaflet';
 
 // css
-import 'leaflet/dist/leaflet.css'
-import '../css/MainView.css'
+import 'leaflet/dist/leaflet.css';
+import '../css/MainView.css';
 
 /**
  * displays leaflet map of data in store.visits
  * can't use 'map' because that is tied to nativeCode
- * 
+ *
  * Created by David Goldstein on 2/9/18
  **/
 
 const styles = {
-  leafletMap : {
-    height : 400,
-    width : 1000,
-    padding : "100px"
-  },
-}
+  leafletMap: {
+    height: 400,
+    width: 1000,
+    padding: '100px'
+  }
+};
 
-const startingPosition = [38.87900161743164, -76.98979949951172]
-const startingZoom = 10
+const startingPosition = [38.87900161743164, -76.98979949951172];
+const startingZoom = 10;
 
 class MapComponent extends React.Component {
-
-	render() {
-		return (
+  render() {
+    return (
       <div>
-      <h1> Showing {this.props.visits.length} visits to your website</h1>
-      <div className={'map-component'} id={'map-component'}>
-      <Map clas={'leaflet-map'} zoom={startingZoom} center={startingPosition} style={styles.leafletMap}>
-        <TileLayer
-          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={startingPosition}>
-          <Popup>
-            <span>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </span>
-          </Popup>
-        </Marker>
-      </Map>
-      </div>
+        <h1> Showing {this.props.visits.length} visits to your website</h1>
+        <div className={'map-component'} id={'map-component'}>
+          <Map
+            clas={'leaflet-map'}
+            zoom={startingZoom}
+            center={startingPosition}
+            style={styles.leafletMap}
+          >
+            <TileLayer
+              attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {this.props.visits.map((visit, id) => {
+              return (
+                <CircleMarker center={[visit.latitude, visit.longitude]} color="red">
+                  <Marker position={[visit.latitude, visit.longitude]}>
+                    <Popup>
+                      <span>{JSON.stringify(visit, null, 2)}</span>
+                    </Popup>
+                  </Marker>
+                </CircleMarker>
+              );
+            })}
+          </Map>
+        </div>
       </div>
     );
-	}
+  }
 }
 
 MapComponent.propTypes = {
-  visits : PropTypes.array.isRequired
-}
+  visits: PropTypes.array.isRequired
+};
 
 export default MapComponent;
